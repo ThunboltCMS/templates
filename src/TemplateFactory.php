ee9d58e90927f80397f2dabfe2ace2d8b84ce230
MartkCz <antik494@gmail.com>
2017-05-27 21:35:49 +0200

    assets manager in template as provider

--- src/TemplateFactory.php
+++ src/TemplateFactory.php
@@ -102,7 +102,8 @@ class TemplateFactory extends ApplicationLatte\TemplateFactory implements ITempl
 			$template->config = $this->config->getValues();
 		}
 		$template->imageStorage = $this->imageStorage;
-		$template->assetsManager = $this->assetsManager;
+
+		$latte->addProvider('assetsManager', $this->assetsManager);
 		if (class_exists(ComposerDirectories::class)) {
 			$template->pluginPath = $template->basePath . '/' . ComposerDirectories::PLUGIN_ASSETS_DIR;
 		}
